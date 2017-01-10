d4104c9766b9645ede83e0d08dcb6206b7dbafde
SkayLarsen <SkayLarsen@gmail.com>
2017-01-10 23:03:43 +0300

    resize_image fix

--- image_resize.py
+++ image_resize.py
@@ -13,24 +13,25 @@ def init_argparser():
     return parser
 
 
+def print_scale_warning():
+    print("Предупреждение: новые пропорции не совпадают с исходным изображением.")
+
+
 def resize_image(path_to_original, width, height, scale):
     image = Image.open(path_to_original)
     original_width, original_height = image.size
     proportion = original_width / original_height
     if scale:
-        resized_image = image.resize([round(original_width * scale), round(original_height * scale)])
+        image = image.resize([round(original_width * scale), round(original_height * scale)])
     elif width and height:
         if width / height != proportion:
-            print("Предупреждение: новые пропорции не совпадают с исходным изображением.")
-        resized_image = image.resize([width, height])
+            print_scale_warning()
+        image = image.resize([width, height])
     elif width:
-        resized_image = image.resize([width, round(width / proportion)])
+        image = image.resize([width, round(width / proportion)])
     elif height:
-        resized_image = image.resize([round(height * proportion), height])
-    else:
-        print("Указано недостаточно опций")
-        return None
-    return resized_image
+        image = image.resize([round(height * proportion), height])
+    return image
 
 
 def save_image(image, path_to_original, path_to_result):
@@ -43,10 +44,12 @@ def save_image(image, path_to_original, path_to_result):
 if __name__ == '__main__':
     argparser = init_argparser()
     args = argparser.parse_args()
-    if args.scale and (args.width or args.height):
-        print("Нельзя одновременно указывать и коэффициент масштабирования, и ширину/высоту")
-    elif not os.path.exists(args.input):
+    if not os.path.exists(args.input):
         print("Указанный файл не найден")
+    elif not (args.width or args.height or args.scale):
+        print("Указано недостаточно опций")
+    elif args.scale and (args.width or args.height):
+        print("Нельзя одновременно указывать коэффициент масштабирования и ширину/высоту")
     else:
         new_image = resize_image(args.input, args.width, args.height, args.scale)
         save_image(new_image, args.input, args.output)
